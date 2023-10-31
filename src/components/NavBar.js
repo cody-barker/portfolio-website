9622f7dfdc72db97209e8519e06da1067b288e82
Cody <codybarker.or@gmail.com>
2023-10-31 10:33:59 -0700

    remove ul and lis from navbar to remove bullets

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,9 @@ import {NavLink} from 'react-router-dom'
 function NavBar() {
     return(
         <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contact">Contact</NavLink>
-                </li>
-            </ul>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
         </nav>
     )
 }
