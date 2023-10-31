07f1b92329a68ee7743f111404f7f9e90972da62
Cody <codybarker.or@gmail.com>
2023-10-31 12:46:34 -0700

    style navbar to the left as column

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,11 @@ function Home() {
   return(
     <div className="container">
       <img src={headshot} alt="headshot"/>
+      <div className="home-header">
+        <p>Hello! I'm</p>
+        <h1>Cody Barker</h1>
+        <p>A Full-Stack Software Engineer with expertise in React, JavaScript, and Ruby on Rails.</p>
+      </div>
     </div>
   )
 }
