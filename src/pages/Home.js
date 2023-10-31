89ee002ef5c07866be4b12f628a9ea180287fe31
Cody <codybarker.or@gmail.com>
2023-10-31 11:41:23 -0700

    style headshot size and radius

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import headshot from '../assets/Cody_Barker_Headshot.jpg'
 function Home() {
   return(
     <div className="container">
-      <h1>cb.</h1>
       <img src={headshot} alt="headshot"/>
     </div>
   )
