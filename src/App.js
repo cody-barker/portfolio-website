ec1192e5acbc6d3e26ede66385838cd3bf0d1371
Cody <codybarker.or@gmail.com>
2023-10-31 16:46:03 -0700

    remove contact

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import NavBar from './components/NavBar'
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
 
 
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
-        <Route path="/contact" element={<Contact />}/>
         <Route path="*" element={<NotFound />}/>
       </Routes>
     </main>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,12 @@
 import {NavLink} from 'react-router-dom'
 import homeIcon from '../assets/home-icon.svg'
 import userIcon from '../assets/user-icon.svg'
-import contactIcon from '../assets/contact-icon.svg'
 
 function NavBar() {
     return(
         <nav>
             <NavLink className="nav-link" to="/"><img src={homeIcon} alt="home icon"/></NavLink>
             <NavLink className="nav-link" to="/about"><img src={userIcon} alt="about me icon"/></NavLink>
-            <NavLink className="nav-link" to="/contact"><img src={contactIcon} alt="contact icon" /></NavLink>
         </nav>
     )
 }
