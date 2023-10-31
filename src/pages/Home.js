1749f7068e3c1f92c50781dcd01710ac5cc43112
Cody <codybarker.or@gmail.com>
2023-10-31 15:07:14 -0700

    add links to home page buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import headshot from '../assets/Cody_Barker_Headshot.jpg'
 
 function Home() {
 
-  function onResumeBtnClick(url) {
+  function onBtnClick(url) {
     window.open(url, '_blank')
   }
 
@@ -14,10 +14,10 @@ function Home() {
         <h1>Cody Barker</h1>
         <p>A Full-Stack Software Engineer with a focus on React, JavaScript, and Ruby on Rails.</p>
         <div className="home-buttons-container">
-          <button onClick={() => onResumeBtnClick('https://drive.google.com/file/d/1n2ZNSdGDth-0gedkApnKqj9REJDzobSM/view?usp=sharing')}>Resume</button>
-          <button>LinkedIn</button>
-          <button>GitHub</button>
-          <button>Dev.to</button>
+          <button onClick={() => onBtnClick('https://drive.google.com/file/d/1n2ZNSdGDth-0gedkApnKqj9REJDzobSM/view?usp=sharing')}>Resume</button>
+          <button onClick={() => onBtnClick('https://www.linkedin.com/in/barkercody/')}>LinkedIn</button>
+          <button onClick={() => onBtnClick('https://github.com/cody-barker')}>Github</button>
+          <button onClick={() => onBtnClick('https://dev.to/codybarker')}>Dev.to</button>
         </div>
       </div>
     </div>
