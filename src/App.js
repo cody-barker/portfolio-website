882b13f9c6d839a49c8e2de7862b574b81e8e787
Cody <codybarker.or@gmail.com>
2023-10-31 10:42:56 -0700

    create and import About page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './styles/App.css';
 import NavBar from './components/NavBar'
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+
 
 function App() {
   return (
     <main>
       <NavBar />
-      <div>Hello World</div>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
     </main>
   );
 }
