af9c8edf38b0c49b2806d69f76df11f2acf35060
Cody <codybarker.or@gmail.com>
2023-10-31 16:27:10 -0700

    change navlink text to icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,14 @@
 import {NavLink} from 'react-router-dom'
+import homeIcon from '../assets/home-icon.svg'
+import userIcon from '../assets/user-icon.svg'
+import contactIcon from '../assets/contact-icon.svg'
 
 function NavBar() {
     return(
         <nav>
-            <NavLink className="nav-link" to="/">Home</NavLink>
-            <NavLink className="nav-link" to="/about">About</NavLink>
-            <NavLink className="nav-link" to="/contact">Contact</NavLink>
+            <NavLink className="nav-link" to="/"><img src={homeIcon} alt="home icon"/></NavLink>
+            <NavLink className="nav-link" to="/about"><img src={userIcon} alt="about me icon"/></NavLink>
+            <NavLink className="nav-link" to="/contact"><img src={contactIcon} alt="contact icon" /></NavLink>
         </nav>
     )
 }
