5c7b93357d5b4679cf8c52d1fbf28dd6383cf523
Cody <codybarker.or@gmail.com>
2023-10-31 17:15:39 -0700

    adjust margin for home icons

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,8 +5,12 @@ function About() {
     <div className="container">
       <div className="about-container">
         <h2>hi, i'm cody!</h2>
-        <p className="about-content">I'm a Full-Stack Software Engineer, experienced with React, JavaScript, Ruby on Rails, and more. I graduated from Flatiron School, where I built projects combining React frontends and Ruby on Rails backends. Prior to my software engineering career, I served as Director of Cultivation at Infinite Tree, where I grew the company into a market leader. I managed the production and sale of over 200,000 USDA organic nursery plants, developed and released 4 industry-leading, university tested cultivars, conducted 15+ scientific cultivation studies, and provided consulting services to farms and government organizations domestically and internationally. I also have extensive experience in ecological restoration and am passionate about the intersection of sustainability, technology, culture, and the environment. My desire is to create applications that connect users with their local environment, driving education and engagement in the world around them. I'm compassionate, professional, reliable, and I'm eager to apply my proven project management and technical skills to the software industry.
-          </p>
+        <p className="about-content">I'm a Full-Stack Software Engineer, experienced with React, JavaScript, Ruby on Rails, and more. I graduated from Flatiron School, where I built projects combining React frontends and Ruby on Rails backends. 
+        </p>
+        <p>Prior to my software engineering career, I served as Director of Cultivation at Infinite Tree, where I managed the production and sale of over 200,000 USDA organic nursery plants, developed and released 4 industry-leading, university tested cultivars, conducted 15+ scientific cultivation studies, and provided consulting services to farms and government organizations domestically and internationally. 
+        </p>
+        <p>I also have extensive experience in ecological restoration and am passionate about the intersection of sustainability, technology, culture, and the environment. My desire is to create applications that connect users with their local environment, driving education and engagement in the world around us. I'm compassionate, professional, reliable, and eager to apply my proven project management and technical skills to the software industry.
+        </p>
       </div>
     </div>
   )
